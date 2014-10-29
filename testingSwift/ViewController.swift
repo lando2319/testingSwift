dc7ccdacbbc003be5e947c47d1f3eb662e98f581
Mike Land <mikepland@gmail.com>
2014-10-29 09:41:25 -0500

    Initial Commit

---
+++ testingSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testingSwift
+//
+//  Created by MIKE LAND on 10/29/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
